bbe9e8e938dc1cd75f775bc6a61ea49d9cb72cb8
farhan-haroon <theoriginalhybrid9@gmail.com>
2022-12-08 16:45:18 +0530

    last

--- src/move_robot/scripts/mover.py
+++ src/move_robot/scripts/mover.py
@@ -12,39 +12,59 @@ class mover(path_planning):
     def __init__(self):
         super().__init__()
         self.path = super().a_star()
-        # dir1 = super().start_dir
-        # dir2 = super().end_dir
+        self.dir1 = self.start_dir
+        self.dir2 = self.end_dir
         # print(path)
         # print(len(path))
         self.motion()
         
     def motion(self):
-        pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         rospy.init_node('mover', anonymous=True)
-        # rate = rospy.Rate(10) # 10hz
-        l = len(self.path)
+        cmd = Twist()
+        
         print("\nPlace the TurtleBot facing North\n")
         ok = input()
 
-        
+        if self.start_dir == "S":
+           print("Turning 180")
+
+        elif self.start_dir == "E":
+            print("Turning 90")
+            
+        elif self.start_dir == "W":
+            print("Turning 270")
+
+        l = len(self.path)
+        old_dir = self.dir1
+
         while not rospy.is_shutdown():
 
-            if self.start_dir == "S":
-                print("turning 180")
-            elif self.start_dir == "E":
-                print("turning 90")
-            elif self.start_dir == "W":
-                print("turning 270")
+            for i in range (1, len):
+                current = self.path[i]
+                previous = self.path[i - 1]
+                
+                if (current[0] == previous[0] and current[1] == (previous[1] + 1)):
+                    new_dir = "E"
+                elif (current[0] == previous[0] and current[1] == (previous[1] - 1)):
+                    new_dir = "W"
+                elif (current[0] == (previous[0] + 1) and current[1] == previous[1]):
+                    new_dir = "S"
+                elif(current[0] == (previous[0] - 1) and current[1] == previous[1]):
+                    new_dir = "N"
 
-            
+                if new_dir == old_dir:
+                    cmd.linear.x = 1.0
+                    publisher.publish(cmd)
+                    time.sleep(2)
 
+                elif (new_dir == "E"):
+                    cmd.angular.z = 1.0
+                    publisher.publish(cmd)
+                    time.sleep(2)
 
-            # for i in range (1, len):
-            #     current = self.path[i]
-            #     previous = self.path[i - 1]
-            #     if (current[0] == previous[0] and current[1] == (previous[1] + 1)):
-
-
+                elif (new_dir == "W"):
+                    cmd.angular.z = 
 
 
 def main():
