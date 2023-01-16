044464cd962a0aac4e072128b2fad03cbc7ef1a6
Farhan Haroon <109333218+farhan-haroon@users.noreply.github.com>
2023-01-16 15:35:07 +0530

    Update scratch.py

--- src/move_robot/scripts/scratch.py
+++ src/move_robot/scripts/scratch.py
@@ -1,20 +1,41 @@
 #!/usr/bin/env python3
 from map_maker import map
-import time
-import numpy as np
+import math
 
+grid = map.nim
 
 class Node():
 
-    def __init__(self, parent, x, y):
+    def __init__(self, parent_x, parent_y, pos_x, pos_y):
         
-        self.parent = parent
-        self.x = x
-        self.y = y
+        self.parent_x = parent_x
+        self.parent_y = parent_y
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+
+def is_movable(row, col):
+    
+    if (grid[row][col] == 1):
+        return False
+    else:
+        return True
+
+
+def is_destination(row, col, dest_r, dest_c):
+
+    if (grid[row][col] == grid[dest_r][dest_c]):
+        return True
+    else:
+        return False
+
+
+def get_gcost(row, col, st_x, st_y):
+
+    cost = math.sqrt()
 
 def path_finder():
 
-    grid = map.nim
     open_list = []
     closed_list = []
     print("Enter Start Co-ordinates: ")
@@ -23,10 +44,17 @@ def path_finder():
     print("Enter End Co-rdinates: ")
     en_x = input("Enter X: ")
     en_y = input("Enter Y: ")
-    start_node = Node(None, st_x, st_y)
-    end_node = Node(None, en_x, en_y)
-    print("Start Node: ", start_node.x, start_node.y)
-    print("Start Node Parent: ", start_node.parent)
+    start_node = Node(None, None, st_x, st_y)
+    end_node = Node(None, None, en_x, en_y)
+    #print("Start Node: ", start_node.pos_x, start_node.pos_y)
+    #print("Start Node Parent: ", start_node.parent_x, start_node.parent_y)
+
+
+
+
+
+
+
 
 def main():
 
