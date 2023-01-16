a8a48662647aef55470e96323add5526166ed9a5
Farhan Haroon <109333218+farhan-haroon@users.noreply.github.com>
2023-01-16 17:49:53 +0530

    Update scratch.py

--- src/move_robot/scripts/scratch.py
+++ src/move_robot/scripts/scratch.py
@@ -1,65 +1,115 @@
-#!/usr/bin/env python3
+#/!usr/bin/env python3
 from map_maker import map
-import math
-
-grid = map.nim
 
 class Node():
 
-    def __init__(self, parent_x, parent_y, pos_x, pos_y):
-        
-        self.parent_x = parent_x
-        self.parent_y = parent_y
-        self.pos_x = pos_x
-        self.pos_y = pos_y
+    def __init__(self, parent, position):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
 
 
-def is_movable(row, col):
-    
-    if (grid[row][col] == 1):
-        return False
-    else:
-        return True
+def path_finder(maze, start, end):
 
-
-def is_destination(row, col, dest_r, dest_c):
-
-    if (grid[row][col] == grid[dest_r][dest_c]):
-        return True
-    else:
-        return False
-
-
-def get_gcost(row, col, st_x, st_y):
-
-    cost = math.sqrt()
-
-def path_finder():
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
 
     open_list = []
     closed_list = []
-    print("Enter Start Co-ordinates: ")
-    st_x = input("Enter X: ")
-    st_y = input("Enter Y: ")
-    print("Enter End Co-rdinates: ")
-    en_x = input("Enter X: ")
-    en_y = input("Enter Y: ")
-    start_node = Node(None, None, st_x, st_y)
-    end_node = Node(None, None, en_x, en_y)
-    #print("Start Node: ", start_node.pos_x, start_node.pos_y)
-    #print("Start Node Parent: ", start_node.parent_x, start_node.parent_y)
 
 
+    open_list.append(start_node)
 
+    # Loop until you find the end
+    while len(open_list) > 0:
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
 
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
 
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
 
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+            # Get node position
+            # print("Current Node Pos: ", type(current_node.position[0]))
+            # print("New Position: ", type(new_position[0]))
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 1:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
 
 
 def main():
 
-    path_finder()
+    maze = map.nim
+    print("Enter Start Co-ordinates: ")
+    st_x = int(input("Enter X: "))
+    st_y = int(input("Enter Y: "))
+    start = (st_x, st_y)
+    print("Enter End Co-ordinates: ")
+    en_x = int(input("Enter X: "))
+    en_y = int(input("Enter Y: "))
+    end = (en_x, en_y)
+    path = path_finder(maze, start, end)
+    print(path)
 
-if __name__ == "__main__":
-    
+
+if __name__ == '__main__':
     main()
