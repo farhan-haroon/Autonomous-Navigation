c9c0ca9de5fc49746767b0d837f7b9ae74c4f182
Farhan Haroon <109333218+farhan-haroon@users.noreply.github.com>
2023-01-23 12:31:33 +0530

    Update odom_sub.py

--- src/move_robot/scripts/odom_sub.py
+++ src/move_robot/scripts/odom_sub.py
@@ -2,249 +2,279 @@
 
 # Importing the necessary libraries
 import math
-#import rospy
-#from geometry_msgs.msg import Twist
+import rospy
+from geometry_msgs.msg import Twist
 
-# Declarations and initialisations
-previous_dir = "NE"
-current_dir = ""
-path = [(41, 15), (40, 16), (39, 17), (38, 18), (37, 19), (36, 20), (35, 21), (35, 22), (35, 23), (35, 24), (35, 25), (35, 26), (34, 27), (33, 28), (32, 29), (31, 30), (30, 31), (29, 32), (28, 33), (27, 34), (26, 35), (25, 36), (24, 36), (23, 36), (22, 35), (21, 35), (20, 35), (19, 35), (18, 36), (17, 37), (16, 38), (15, 39), (15, 40), (15, 41), (15, 42), (15, 43), (15, 44), (15, 45)]
-l = len(path)
-a = b = 0
+class mover():
 
-# ros node, publisher, and rate declarations
-#pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
-#rospy.init_node('move_to_location')
-#rate = rospy.Rate(5)
+    def __init__(self, path,):
 
-# Function to move the robot straight
-def move_by(val, t):
-    print("\nMoving by ", val)
-    print("For time: ", t,"\n")
+        rospy.init_node('robot_mover')
+        self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
+        rate = rospy.Rate(5)
+        self.path = path
 
-# Function to turn the robot left
-def turn_left(deg):
-    print("\tTurning left by: ", deg, "\n")
-    if deg == 45:
-        print("\t& moving by 1 diagonal step\n")
-    elif deg == 90:
-        print ("\t& moving by 1 straight step\n")
-
-# Function to turn the robot right 
-def turn_right(deg):
-    print("\tTurning right by: ", deg, "\n")
-    if deg == 45:
-        print("\t& moving by 1 diagonal step\n")
-    elif deg == 90:
-        print ("\t& moving by 1 straight step\n")
-
-# Function to get the current direction by getting the previous direction
-def get_direction(current, previous):
-
-    if (current[0] == previous[0] and current[1] == previous[1] + 1):
-        return ("E")
-    elif (current[0] == previous[0] and current[1] == previous[1] - 1):
-        return ("W")
-    elif (current[0] == previous[0] + 1 and current[1] == previous[1]):
-        return ("S")
-    elif (current[0] == previous[0] - 1 and current[1] == previous[1]):
-        return ("N")
-    elif (current[0] == previous[0] - 1 and current[1] == previous[1] + 1):
-        return ("NE")
-    elif (current[0] == previous[0] + 1 and current[1] == previous[1] + 1):
-        return ("SE")
-    elif (current[0] == previous[0] - 1 and current[1] == previous[1] - 1):
-        return ("NW")
-    elif (current[0] == previous[0] + 1 and current[1] == previous[1] - 1):
-        return ("SW")
-
-
-# main computation
-for i in range (0, l - 1):
-
-    # Getting the current and the previous nodes from the path
-    previous = path[i]
-    current = path[i + 1]
-
-    # Getting the current directions by comparing with the previous directions
-    current_dir = get_direction(current, previous)
-
-    # For same previous and current directions
-    if current_dir == previous_dir:
-        if current_dir in ("N", "S", "E", "W"):
-            a = a + 1
-        elif current_dir in ("NE", "NW", "SE", "SW"):
-            b = b + 1
+    # Function to move the robot straight
+    def move_by(self, val, t):
+        if (val == 1):
+            dist = 0.045 * t
+        else:
+            dist = 0.045 * math.sqrt(2) * t
         
-        print("Previous direction: ", previous_dir, "\n")
-        print("Previous node: ", previous,"\n")
-        print("Current direction: ", current_dir, "\n")
-        print("Currrent node: ", current,"\n")
-        if current != path[len(path) - 1]:
-            previous_dir = current_dir
-            continue
+        time = dist / 0.05
+        end_time = rospy.get_time() + time
+        msg = Twist()
+
+        print("\nMoving by ", dist, "m")
+        print("For time: ", time,"s")
+        print("At velocity: 0.05 m/s")
+        print("Times: ",t, "\n")
+
+        while rospy.get_time() < end_time:
+            msg.linear.x = 0.05
+            self.pub.publish(msg)
+
+
+    # Function to turn the robot left
+    def turn_left(self, deg):
+        print("\tTurning left by: ", deg, "\n")
+        if deg == 45:
+            print("\t& moving by 1 diagonal step\n")
+        elif deg == 90:
+            print ("\t& moving by 1 straight step\n")
+
+    # Function to turn the robot right 
+    def turn_right(self, deg):
+        print("\tTurning right by: ", deg, "\n")
+        if deg == 45:
+            print("\t& moving by 1 diagonal step\n")
+        elif deg == 90:
+            print ("\t& moving by 1 straight step\n")
+
+    # Function to get the current direction by getting the previous direction
+    def get_direction(self, current, previous):
+
+        if (current[0] == previous[0] and current[1] == previous[1] + 1):
+            return ("E")
+        elif (current[0] == previous[0] and current[1] == previous[1] - 1):
+            return ("W")
+        elif (current[0] == previous[0] + 1 and current[1] == previous[1]):
+            return ("S")
+        elif (current[0] == previous[0] - 1 and current[1] == previous[1]):
+            return ("N")
+        elif (current[0] == previous[0] - 1 and current[1] == previous[1] + 1):
+            return ("NE")
+        elif (current[0] == previous[0] + 1 and current[1] == previous[1] + 1):
+            return ("SE")
+        elif (current[0] == previous[0] - 1 and current[1] == previous[1] - 1):
+            return ("NW")
+        elif (current[0] == previous[0] + 1 and current[1] == previous[1] - 1):
+            return ("SW")
+
+    def robot_mover(self):
         
-    if ((current_dir != previous_dir) or current == path[len(path) - 1]):
-        if a > 0:
-            move_by(1, a)
-        elif b > 0:
-            move_by(math.sqrt(2), b)
+        previous_dir = "NE"
+        current_dir = ""
+        l = len(self.path)
         a = b = 0
-        if current == path[len(path) - 1]:
-            break
 
-    print("Previous direction: ", previous_dir, "\n")
-    print("Previous node: ", previous,"\n")
-    print("Current direction: ", current_dir, "\n")
-    print("Currrent node: ", current,"\n")
+        # main computation
+        for i in range (0, l - 1):
+            
+            # Getting the current and the previous nodes from the path
+            previous = self.path[i]
+            current = self.path[i + 1]
+
+            # Getting the current directions by comparing with the previous directions
+            current_dir = self.get_direction(current, previous)
+
+            # For same previous and current directions
+            if current_dir == previous_dir:
+                if current_dir in ("N", "S", "E", "W"):
+                    a = a + 1
+                elif current_dir in ("NE", "NW", "SE", "SW"):
+                    b = b + 1
+                print("Previous direction: ", previous_dir, "\n")
+                print("Previous node: ", previous,"\n")
+                print("Current direction: ", current_dir, "\n")
+                print("Currrent node: ", current,"\n")
+                if current != self.path[len(self.path) - 1]:
+                    previous_dir = current_dir
+                    continue
+
+            if ((current_dir != previous_dir) or current == self.path[len(self.path) - 1]):
+                if a > 0:
+                    self.move_by(1, a)
+                elif b > 0:
+                    self.move_by(math.sqrt(2), b)
+                a = b = 0
+                if current == self.path[len(self.path) - 1]:
+                    break
+
+            print("Previous direction: ", previous_dir, "\n")
+            print("Previous node: ", previous,"\n")
+            print("Current direction: ", current_dir, "\n")
+            print("Currrent node: ", current,"\n")
+
+            # For straight previous directions
+
+            if previous_dir == "N":
+
+                if current_dir == "NE":
+                    self.turn_right(45)
+                    b = b + 1
+                elif current_dir == "NW":math
+#import rospy
+                    self.turn_left(45)
+                    b = b + 1
+                elif current_dir == "E":
+                    self.turn_right(90)
+                    a = a + 1
+                elif current_dir == "W":
+                    self.turn_left(90)
+                    a = a + 1
+
+                previous_dir = current_dir
+                continue
 
 
-    # For straight previous directions
+            if previous_dir == "S":
 
-    if previous_dir == "N":
+                if current_dir == "SE":
+                    self.turn_left(45)
+                    b = b + 1
+                elif current_dir == "SW":
+                    self.turn_right(45)
+                    b = b + 1
+                elif current_dir == "E":
+                    self.turn_left(90)
+                    a = a + 1
+                elif current_dir == "W":
+                    self.turn_right(90)
+                    a = a + 1
 
-        if current_dir == "NE":
-            turn_right(45)
-            b = b + 1
-        elif current_dir == "NW":
-            turn_left(45)
-            b = b + 1
-        elif current_dir == "E":
-            turn_right(90)
-            a = a + 1
-        elif current_dir == "W":
-            turn_left(90)
-            a = a + 1
+                previous_dir = current_dir
+                continue
 
-        previous_dir = current_dir
-        continue
+            if previous_dir == "E":
 
-    if previous_dir == "S":
+                if current_dir == "NE":
+                    self.turn_left(45)
+                    b = b + 1
+                elif current_dir == "SE":
+                    self.turn_right(45)
+                    b = b + 1
+                elif current_dir == "N":
+                    self.turn_left(90)
+                    a = a + 1
+                elif current_dir == "S":
+                    self.turn_right(90)math
+#import rospy
+                    a = a + 1
 
-        if current_dir == "SE":
-            turn_left(45)
-            b = b + 1
-        elif current_dir == "SW":
-            turn_right(45)
-            b = b + 1
-        elif current_dir == "E":
-            turn_left(90)
-            a = a + 1
-        elif current_dir == "W":
-            turn_right(90)
-            a = a + 1
+                previous_dir = current_dir
+                continue
 
-        previous_dir = current_dir
-        continue
+            if previous_dir == "W":
 
-    if previous_dir == "E":
+                if current_dir == "NW":
+                    self.turn_right(45)
+                    b = b + 1
+                elif current_dir == "SW":
+                    self.turn_left(45)
+                    b = b + 1
+                elif current_dir == "N":
+                    self.turn_right(90)
+                    a = a + 1
+                elif current_dir == "S":
+                    self.turn_left(90)
+                    a = a + 1
 
-        if current_dir == "NE":
-            turn_left(45)
-            b = b + 1
-        elif current_dir == "SE":
-            turn_right(45)
-            b = b + 1
-        elif current_dir == "N":
-            turn_left(90)
-            a = a + 1
-        elif current_dir == "S":
-            turn_right(90)
-            a = a + 1
+                previous_dir = current_dir
+                continue
 
-        previous_dir = current_dir
-        continue
+            # For diagonal previous directions
 
-    if previous_dir == "W":
+            if previous_dir == "NE":
 
-        if current_dir == "NW":
-            turn_right(45)
-            b = b + 1
-        elif current_dir == "SW":
-            turn_left(45)
-            b = b + 1
-        elif current_dir == "N":
-            turn_right(90)
-            a = a + 1
-        elif current_dir == "S":
-            turn_left(90)
-            a = a + 1
+                if current_dir == "N":
+                    self.turn_left(45)
+                    b = b + 1
+                elif current_dir == "NW":
+                    self.turn_left(90)
+                    a = a + 1
+                elif current_dir == "E":
+                    self.turn_right(45)
+                    b = b + 1
+                elif current_dir == "SE":
+                    self.turn_right(90)math
+#import rospy
+                    a = a + 1
 
-        previous_dir = current_dir
-        continue
+                previous_dir = current_dir
+                continue
 
-# For diagonal previous directions
+            if previous_dir == "NW":
 
-    if previous_dir == "NE":
+                if current_dir == "NE":
+                    self.turn_right(90)
+                    a = a + 1
+                elif current_dir == "SW":
+                    self.turn_left(90)
+                    a = a + 1
+                elif current_dir == "N":
+                    self.turn_right(45)
+                    b = b + 1
+                elif current_dir == "W":
+                    self.turn_left(45)
+                    b = b + 1
 
-        if current_dir == "N":
-            turn_left(45)
-            b = b + 1
-        elif current_dir == "NW":
-            turn_left(90)
-            a = a + 1
-        elif current_dir == "E":
-            turn_right(45)
-            b = b + 1
-        elif current_dir == "SE":
-            turn_right(90)
-            a = a + 1
+                previous_dir = current_dir
+                continue
 
-        previous_dir = current_dir
-        continue
+            if previous_dir == "SE":
 
-    if previous_dir == "NW":
+                if current_dir == "NE":
+                    self.turn_left(90)
+                    a = a + 1
+                elif current_dir == "SW":
+                    self.turn_right(90)
+                    a = a + 1
+                elif current_dir == "E":math
+#import rospy
+                    self.turn_left(45)
+                    b = b + 1
+                elif current_dir == "S":
+                    self.turn_right(45)
+                    b = b + 1
 
-        if current_dir == "NE":
-            turn_right(90)
-            a = a + 1
-        elif current_dir == "SW":
-            turn_left(90)
-            a = a + 1
-        elif current_dir == "N":
-            turn_right(45)
-            b = b + 1
-        elif current_dir == "W":
-            turn_left(45)
-            b = b + 1
+                previous_dir = current_dir
+                continue
 
-        previous_dir = current_dir
-        continue
+            if previous_dir == "SW":
 
-    if previous_dir == "SE":
+                if current_dir == "NW":
+                    self.turn_right(90)
+                    a = a + 1
+                elif current_dir == "SE":
+                    self.turn_left(90)
+                    a = a + 1
+                elif current_dir == "W":
+                    self.turn_right(45)
+                    b = b + 1
+                elif current_dir == "S":
+                    self.turn_left(45)
+                    b = b + 1
 
-        if current_dir == "NE":
-            turn_left(90)
-            a = a + 1
-        elif current_dir == "SW":
-            turn_right(90)
-            a = a + 1
-        elif current_dir == "E":
-            turn_left(45)
-            b = b + 1
-        elif current_dir == "S":
-            turn_right(45)
-            b = b + 1
+                previous_dir = current_dir
+                continue
 
-        previous_dir = current_dir
-        continue
-    
-    if previous_dir == "SW":
 
-        if current_dir == "NW":
-            turn_right(90)
-            a = a + 1
-        elif current_dir == "SE":
-            turn_left(90)
-            a = a + 1
-        elif current_dir == "W":
-            turn_right(45)
-            b = b + 1
-        elif current_dir == "S":
-            turn_left(45)
-            b = b + 1
+def main():
+    way = [(41, 15), (40, 16), (39, 17), (38, 18), (37, 19), (36, 20), (35, 21), (35, 22), (35, 23), (35, 24), (35, 25), (35, 26), (34, 27), (33, 28), (32, 29), (31, 30), (30, 31), (29, 32), (28, 33), (27, 34), (26, 35), (25, 36), (24, 36), (23, 36), (22, 35), (21, 35), (20, 35), (19, 35), (18, 36), (17, 37), (16, 38), (15, 39), (15, 40), (15, 41), (15, 42), (15, 43), (15, 44), (15, 45)]
+    obj = mover(way)
+    obj.robot_mover()
+    print("Destination reached.")
 
-        previous_dir = current_dir
-        continue
-    
-    
+if __name__ == '__main__':
+    main()
