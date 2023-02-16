a2b4292e303b644b63a4f2e8ac06df578f44b4ae
Farhan Haroon <109333218+farhan-haroon@users.noreply.github.com>
2023-02-16 16:29:51 +0530

    Update auto_nav.py

--- src/move_robot/scripts/auto_nav.py
+++ src/move_robot/scripts/auto_nav.py
@@ -13,6 +13,7 @@ c = 0
 def callback_method(msg):
     global current_x, current_y, current_yaw
     current_x = msg.pose.pose.position.x
+
     current_y = msg.pose.pose.position.y
     quaternion = (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z,
                   msg.pose.pose.orientation.w)
@@ -24,8 +25,8 @@ def mover(goal_x, goal_y):
     global current_x, current_y, c
 
     p_controller_linear = 0.5
-    p_controller_angular = 0.8
-
+    # p_controller_angular = 0.8
+    rate = rospy.Rate(10)
     speed = Twist()
 
     while not rospy.is_shutdown():
@@ -42,20 +43,29 @@ def mover(goal_x, goal_y):
                 linear_speed = dist * p_controller_linear
 
                 # set upper limit of linear velocity
-                linear_speed = min(linear_speed, 0.06)
+                linear_speed = min(linear_speed, 0.1)
                 # set lower limit of linear velocity
-                linear_speed = max(linear_speed, 0.04)
+                linear_speed = max(linear_speed, 0.06)
 
                 angle_to_goal = math.atan2(
                     goal_y - current_y, goal_x - current_x)
 
-                angular_speed = (angle_to_goal - current_yaw) * \
-                    p_controller_angular
+                if (current_yaw < 0):
+                    yaw = 6.28 - abs(current_yaw)
+
+                else:
+                    yaw = current_yaw
+
+                if (angle_to_goal < 0):
+                    angle_to_goal = 6.28 - abs(angle_to_goal)
+
+                delta_heading = math.atan2(
+                    math.sin(angle_to_goal - yaw), math.cos(angle_to_goal - yaw))
 
                 # set upper limit of angular velocity
-                angular_speed = min(angular_speed, 0.1)
+                angular_speed = min(delta_heading, 0.15)
                 # set lower limit of angular velocity
-                angular_speed = max(angular_speed, -0.1)
+                angular_speed = max(delta_heading, -0.15)
 
                 if (angular_speed > 0.05 or angular_speed < -0.05):
                     linear_speed = 0.0
@@ -64,6 +74,8 @@ def mover(goal_x, goal_y):
                 speed.angular.z = angular_speed
 
                 pub.publish(speed)
+                rate.sleep()
+
             break
 
     speed.linear.x = 0.0
@@ -86,13 +98,11 @@ if __name__ == "__main__":
     rospy.Subscriber('/odom', Odometry, callback_method)
 
     cell_size = float(input("Enter the cell size: "))
-    path = [(32, 27), (33, 28), (34, 29), (35, 30), (36, 31), (37, 32), (38, 33), (39, 34), (40, 35), (41, 36), (42, 37), (42, 38), (42, 39), (42, 40), (42, 41), (42, 42), (42, 43), (42, 44), (42, 45), (42, 46), (42, 47), (42, 48), (42, 49), (41, 50), (41, 51), (41, 52), (41, 53), (41, 54), (41, 55), (41, 56), (41, 57), (41, 58), (41, 59), (42, 60),
-          (43, 61), (44, 62), (45, 62), (46, 62), (47, 62), (48, 62), (49, 62), (50, 62), (51, 62), (52, 62), (53, 62), (54, 62), (55, 62), (56, 62), (57, 62), (58, 62), (59, 62), (60, 62), (61, 62), (62, 62), (63, 62), (64, 62), (65, 62), (66, 62), (67, 62), (68, 62), (69, 62), (70, 62), (71, 62), (72, 62), (73, 62), (74, 62), (75, 62), (76, 62)]
 
-    # path = [(76, 62), (75, 61), (74, 60), (73, 59), (72, 58), (71, 57), (70, 56), (69, 55), (70, 54), (70, 53), (70, 52), (70, 51), (70, 50), (70, 49), (70, 48), (69, 47), (68, 46), (67, 45), (66, 44), (65, 43), (64, 42), (63, 41), (62, 40), (62, 39), (62, 38), (62, 37), (62, 36), (62, 35), (61, 34), (60, 33), (59, 32), (58, 31), (57, 30), (56, 29), (55, 28), (54, 27), (53, 27), (52, 27), (51, 27), (50, 27), (49, 27), (48, 27), (47, 27), (46, 27), (45, 27), (44, 27), (43, 27), (42, 27), (41, 27), (40, 27), (39, 27), (38, 27), (37, 27), (36, 27), (35, 27), (34, 27), (33, 27), (32, 27)]
+    path = [(54, 50), (53, 49), (52, 48), (51, 47), (50, 46), (49, 45), (50, 44), (51, 43), (52, 42), (53, 41), (53, 40), (53, 39), (53, 38), (53, 37), (53, 36), (53, 35), (53, 34), (53, 33), (53, 32), (53, 31), (53, 30), (52, 29), (51, 28), (50, 27), (49, 26), (48, 25), (47, 25), (46, 25), (45, 25), (44, 25), (43, 25), (42, 25), (41, 25), (40, 25), (39, 25), (38, 25), (37, 25), (36, 24), (35, 24), (34, 24), (33, 24), (32, 24), (31, 23), (30, 22), (29, 21), (28, 21), (27, 21), (26, 21), (25, 21), (24, 21), (23, 21), (22, 21), (21, 21), (20, 21), (19, 21), (18, 21), (17, 22), (17, 23), (17, 24)]
 
-    x_offset = path[0][0]
-    y_offset = path[0][1]
+    x_offset = int(input("Enter X offset: "))
+    y_offset = int(input("Enter Y offset: "))
 
     print("X-offset =", x_offset)
     print("Y-offset =", y_offset)
